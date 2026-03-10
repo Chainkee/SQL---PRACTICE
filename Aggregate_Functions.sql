cbfd0b41755badbda35c1ae76e1baabc45e1f9f2
Chainkee <frankeli_05@live.com>
2026-03-10 03:11:20 +0000

    Add aggregate queries for film locations

--- Aggregate_Functions.sql
+++ Aggregate_Functions.sql
@@ -2,3 +2,23 @@
 SELECT COUNT(Locations) 
 FROM FilmLocations
 WHERE Writer='James Cameron';
+
+-- Count film directed by Woody Allen
+SELECT COUNT(Title) 
+FROM FilmLocations
+WHERE Director='Woody Allen';
+
+--Count films shot at Russian Hill
+SELECT COUNT(Title) 
+FROM FilmLocations
+WHERE Locations='Russian Hill';
+
+-- Count number of films rreleased before 1950
+SELECT COUNT(ReleaseYear) 
+FROM FilmLocations
+WHERE ReleaseYear < 1950;
+
+-- Count unique release years for Warner Bros. films
+SELECT COUNT (DISTINCT ReleaseYear) 
+FROM FilmLocations
+WHERE ProductionCompany = 'Warner Bros. Pictures';
