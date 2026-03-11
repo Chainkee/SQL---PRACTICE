6bda7761d62c2b4d18a1657b07ad8548a33d0ad8
Chainkee <frankeli_05@live.com>
2026-03-11 02:11:47 +0000

    Delete instructor records for Doe and Hima

---
+++ delete_examples.sql
@@ -0,0 +1,7 @@
+-- Remove the instructor record of Doe whose id is 6
+DELETE FROM instructor
+WHERE ins_id = 6;
+
+-- Remove the instructor record of Hima
+DELETE FROM instructor
+WHERE firstname = 'Hima';
