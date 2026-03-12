7d023c7b07b6b4e6df4461a408273264b60c89a8
Chainkee <frankeli_05@live.com>
2026-03-12 18:57:57 +0000

    Add queries to retrieve employee counts and salaries

--- Grouping.sql
+++ Grouping.sql
@@ -3,3 +3,19 @@ SELECT DEP_ID, COUNT(*)
 FROM EMPLOYEES
 GROUP BY DEP_ID;
 
+-- For each department retrieve the number of employees in the department, and the average employee salary in the department..
+SELECT DEP_ID, COUNT(*), AVG(SALARY)
+FROM EMPLOYEES
+GROUP BY DEP_ID;
+
+-- Label the computed columns in the result above as NUM_EMPLOYEES and AVG_SALARY.
+SELECT DEP_ID, COUNT(*) AS "NUM_EMPLOYEES", AVG(SALARY) AS "AVG_SALARY"
+FROM EMPLOYEES
+GROUP BY DEP_ID;
+
+-- Fron the above, limit the result to departments with fewer than 4 employees.
+SELECT DEP_ID, COUNT(*) AS "NUM_EMPLOYEES", AVG(SALARY) AS "AVG_SALARY"
+FROM EMPLOYEES
+GROUP BY DEP_ID
+HAVING count(*) < 4
+ORDER BY AVG_SALARY;
