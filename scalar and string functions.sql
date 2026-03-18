5b14c799f59469932810880c253ddc73f1f3bda3
Chainkee <frankeli_05@live.com>
2026-03-18 02:26:07 +0000

    Add SQL queries for animal rescue functions

---
+++ scalar and string functions.sql
@@ -0,0 +1,20 @@
+-- Query B1: Enter a function that displays the rounded cost of each rescue.
+SELECT ROUND(COST)
+FROM PETRESCUE
+
+-- Query B2: Enter a function that displays the length of each animal name.
+SELECT LENGTH(ANIMAL)
+FROM PETRESCUE;
+
+-- Query B3: Enter a function that displays the animal name in each rescue in uppercase.
+SELECT UCASE(ANIMAL)
+FROM PETRESCUE;
+
+-- Query B4: Enter a function that displays the animal name in each rescue in uppercase without duplications.
+SELECT DISTINCT UCASE(ANIMAL)
+FROM PETRESCUE;
+
+-- Query B5: Enter a query that displays all the columns from the PETRESCUE table, where the animal(s) rescued are cats. Use cat in lower case in the query.
+SELECT *
+FROM PETRESCUE
+WHERE LOWER(ANIMAL)= 'CAT'
