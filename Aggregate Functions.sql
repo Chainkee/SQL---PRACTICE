6ba43a81ba18f3fa06cc3712f62bf66bf02eebd1
Chainkee <frankeli_05@live.com>
2026-03-18 01:59:19 +0000

    Add aggregate functions for animal rescue data
    Added SQL queries to calculate total cost, maximum quantity, and average cost of animal rescues.

---
+++ Aggregate Functions.sql
@@ -0,0 +1,20 @@
+-- Query A1: Enter a function that calculates the total cost of all animal rescues in the PETRESCUE table.
+SELECT SUM(COST)
+FROM PETRESCUE;
+
+-- Query A2: Enter a function that displays the total cost of all animal rescues in the PETRESCUE table in a column called SUM_OF_COST.
+SELECT SUM(COST) AS SUM_OF_COST
+FROM PETRESCUE;
+
+-- Query A3: Enter a function that displays the maximum quantity of animals rescued.
+SELECT MAX(QUANTITY)
+FROM PETRESCUE;
+
+-- Query A4: Enter a function that displays the average cost of animals rescued.
+SELECT AVG(COST)
+FROM PETRESCUE;
+
+-- Query A5: Enter a function that displays the average cost of rescuing a dog.
+SELECT AVG(COST)
+FROM PETRESCUE
+WHERE ANIMAL = 'DOG';
